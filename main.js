ed18fae13fa9187344ea8ec9cbbad48861f6dc5f
unknown <abbasmurad11>
2020-04-29 11:00:14 +0400

    first v

---
+++ main.js
@@ -0,0 +1,31 @@
+function yonlendir(){ 
+         window.location.href="html.html";
+       }
+
+function yonlendir2(){ 
+         window.location.href="css.html";
+       }
+
+function yonlendir3(){ 
+         window.location.href="js.html";
+       }
+
+function yonlendir4(){
+		window.location.href="b4.html";
+}
+
+function yonlendirmain(){
+		window.location.href="index.html"
+}
+
+function yonlendirelaqe(){
+		window.location.href="index.html#elaqe"
+}
+
+function yonlendirteam(){
+		window.location.href="index.html#team"
+}
+
+function yonlendircompany(){
+		window.location.href="index.html#company"
+}
